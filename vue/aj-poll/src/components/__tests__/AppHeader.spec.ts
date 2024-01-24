e25fa9d3034c2435206948602bda2d7b7a46fce8
ajarombek <ajarombek95@gmail.com>
2024-01-24 11:42:03 -0500

    ui header working

--- vue/aj-poll/src/components/__tests__/AppHeader.spec.ts
+++ vue/aj-poll/src/components/__tests__/AppHeader.spec.ts
@@ -1,9 +1,9 @@
 import { describe, it, expect } from 'vitest'
 
 import { mount } from '@vue/test-utils'
-import Header from '../Header.vue'
+import Header from '../AppHeader.vue'
 
-describe('HelloWorld', () => {
+describe('AppHeader', () => {
   it('renders properly', () => {
     const wrapper = mount(Header)
     expect(wrapper.text()).toContain('AJ Top 25 Poll')
