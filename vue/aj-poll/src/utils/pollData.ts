f37d35835c73cb917b59422d338f85d39a1aeb6c
ajarombek <ajarombek95@gmail.com>
2024-02-17 19:46:09 -0500

    company poll data

--- vue/aj-poll/src/utils/pollData.ts
+++ vue/aj-poll/src/utils/pollData.ts
@@ -305,5 +305,154 @@ export const components: PollEntry[] = [
 ];
 
 export const companies: PollEntry[] = [
-
+  {
+    id: 1,
+    name: 'Microsoft',
+    votes: 51,
+    firstPlaceVotes: 1
+  },
+  {
+    id: 2,
+    name: 'Apple',
+    votes: 46,
+    firstPlaceVotes: 1
+  },
+  {
+    id: 3,
+    name: 'Amazon',
+    votes: 45,
+    firstPlaceVotes: 0
+  },
+  {
+    id: 4,
+    name: 'Alphabet',
+    votes: 45,
+    firstPlaceVotes: 0
+  },
+  {
+    id: 5,
+    name: 'Meta Platforms',
+    votes: 39,
+    firstPlaceVotes: 0
+  },
+  {
+    id: 6,
+    name: 'Nvidia',
+    votes: 28,
+    firstPlaceVotes: 1
+  },
+  {
+    id: 7,
+    name: 'IBM',
+    votes: 25,
+    firstPlaceVotes: 0
+  },
+  {
+    id: 8,
+    name: 'Adobe',
+    votes: 24,
+    firstPlaceVotes: 0
+  },
+  {
+    id: 9,
+    name: 'Intel',
+    votes: 24,
+    firstPlaceVotes: 0
+  },
+  {
+    id: 10,
+    name: 'Databricks',
+    votes: 23,
+    firstPlaceVotes: 0
+  },
+  {
+    id: 11,
+    name: 'Cisco',
+    votes: 21,
+    firstPlaceVotes: 0
+  },
+  {
+    id: 12,
+    name: 'OpenAI',
+    votes: 19,
+    firstPlaceVotes: 0
+  },
+  {
+    id: 13,
+    name: 'Oracle',
+    votes: 19,
+    firstPlaceVotes: 0
+  },
+  {
+    id: 14,
+    name: 'Salesforce',
+    votes: 18,
+    firstPlaceVotes: 0
+  },
+  {
+    id: 15,
+    name: 'Take-Two Interactive',
+    votes: 17,
+    firstPlaceVotes: 0
+  },
+  {
+    id: 16,
+    name: 'AMD',
+    votes: 16,
+    firstPlaceVotes: 0
+  },
+  {
+    id: 17,
+    name: 'SAP',
+    votes: 16,
+    firstPlaceVotes: 0
+  },
+  {
+    id: 18,
+    name: 'TSMC',
+    votes: 15,
+    firstPlaceVotes: 0
+  },
+  {
+    id: 19,
+    name: 'Splunk',
+    votes: 14,
+    firstPlaceVotes: 0
+  },
+  {
+    id: 20,
+    name: 'Hashicorp',
+    votes: 13,
+    firstPlaceVotes: 0
+  },
+  {
+    id: 21,
+    name: 'VMware',
+    votes: 13,
+    firstPlaceVotes: 0
+  },
+  {
+    id: 22,
+    name: 'Okta',
+    votes: 12,
+    firstPlaceVotes: 0
+  },
+  {
+    id: 23,
+    name: 'HP Inc.',
+    votes: 12,
+    firstPlaceVotes: 0
+  },
+  {
+    id: 24,
+    name: 'MongoDB',
+    votes: 11,
+    firstPlaceVotes: 0
+  },
+  {
+    id: 25,
+    name: 'Dell Technologies',
+    votes: 11,
+    firstPlaceVotes: 0
+  },
 ];
