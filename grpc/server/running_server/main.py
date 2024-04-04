8477ca66ed28969e3684c0af5d4bb62536d3fc15
ajarombek <ajarombek95@gmail.com>
2024-04-03 20:35:37 -0400

    format fixes, e2e test attempt

--- grpc/server/running_server/main.py
+++ grpc/server/running_server/main.py
@@ -1,13 +1,13 @@
+import os
 from concurrent import futures
 
 import grpc
 
-from grpc_running_service import running_pb2_grpc, running_pb2
+from grpc_running_service import running_pb2, running_pb2_grpc
 
 
 class RunningMileageService(running_pb2_grpc.RunningServicer):
-    def GetUserWeeklyMileage(self, request, context):
-        # Example implementation; in a real scenario, you'd query a database or another data source
+    def GetUserWeeklyMileage(self, request, context):  # noqa: N802
         weekly_mileages = [
             running_pb2.WeeklyMileage(week="2024-W01", mileage=10.0),
             running_pb2.WeeklyMileage(week="2024-W02", mileage=15.0),
@@ -16,12 +16,16 @@ class RunningMileageService(running_pb2_grpc.RunningServicer):
 
 
 def serve():
+    host = "[::]:50051"
+
     server = grpc.server(futures.ThreadPoolExecutor(max_workers=10))
     running_pb2_grpc.add_RunningServicer_to_server(RunningMileageService(), server)
-    server.add_insecure_port('[::]:50051')
+    server.add_insecure_port(host)
     server.start()
+    print(f"Server started at {host}")
+
     server.wait_for_termination()
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     serve()
--- grpc/service/grpc_running_service/running_pb2_grpc.py
+++ grpc/service/grpc_running_service/running_pb2_grpc.py
@@ -2,7 +2,7 @@
 """Client and server classes corresponding to protobuf-defined services."""
 import grpc
 
-import running_pb2 as running__pb2
+from . import running_pb2 as running__pb2
 
 
 class RunningStub(object):
