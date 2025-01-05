d6a053fabfdaf95ffe9501dbbda924049d14b4a9
Andrew Jarombek <andrew@jarombek.com>
2025-01-05 19:51:29 +0000

    Format Python Code with Black

--- python/dynamic_dispatch/test/test_dynamic_dispatch.py
+++ python/dynamic_dispatch/test/test_dynamic_dispatch.py
@@ -3,21 +3,21 @@ from dynamic_dispatch import A, B, C
 
 def test_class_a():
     a = A()
-    assert a.x() == 'A.x,A.y'
-    assert a.y() == 'A.y'
+    assert a.x() == "A.x,A.y"
+    assert a.y() == "A.y"
 
 
 def test_class_b():
     b = B()
-    assert b.x() == 'A.x,B.y'
-    assert b.y() == 'B.y'
+    assert b.x() == "A.x,B.y"
+    assert b.y() == "B.y"
 
 
 def test_class_c():
     c = C()
-    assert c.x() == 'A.x,B.y'
-    assert c.y() == 'B.y'
-    assert c.z() == 'C.z,A.x,A.y'
+    assert c.x() == "A.x,B.y"
+    assert c.y() == "B.y"
+    assert c.z() == "C.z,A.x,A.y"
 
 
 def test_mro():
