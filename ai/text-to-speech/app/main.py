6dad3d794b3673157fcd46107e8eedf9192ec49b
Andrew Jarombek <andrew@jarombek.com>
2026-01-06 15:08:35 -0500

    Working example

--- ai/text-to-speech/app/main.py
+++ ai/text-to-speech/app/main.py
@@ -1,5 +1,16 @@
-from kokoro import Kokoro
+import soundfile
 
-model = Kokoro.from_pretrained("hexgrad/kokoro-7b")
-output = model.generate("Hello, world!")
-print(output)
+from kokoro import KPipeline
+
+if __name__ == '__main__':
+
+    # Create a pipeline (use 'a' for default American English)
+    pipeline = KPipeline(lang_code="a", repo_id='hexgrad/Kokoro-82M')
+
+    text = "Hello world from Kokoro TTS."
+
+    # Run synthesis and get audio batches
+    for _, _, audio in pipeline(text, voice="af_heart"):
+        # `audio` is a NumPy array at 24 kHz
+        print("Generated audio shape:", audio.shape)
+        soundfile.write("out.wav", audio, 24000)
